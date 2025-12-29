b1c77bbc502b0a8d4ae65a5253c1618b4ff5e3ce
Vst-Byte <matheusgvisentim@gmail.com>
2025-12-29 15:46:51 -0300

    Meu projeto seguro

---
+++ main.tf
@@ -0,0 +1,15 @@
+main.tf
+
+# Configura o "provedor" (quem vai criar a infraestrutura: AWS)
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Cria um Balde S3 (como se fosse uma pasta no Google Drive)
+resource "aws_s3_bucket" "meu_balde_secreto" {
+  bucket = "balde-vst-byte-teste-123" # O nome tem que ser único no mundo todo
+
+  # 🛑 O ERRO ESTÁ AQUI EMBAIXO!
+  # "public-read" deixa qualquer pessoa na internet ler seus arquivos.
+  acl    = "public-read"
+}
